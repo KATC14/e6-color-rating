499279ca7823fde936d4b6360657c7e8f3c0543e
KATC14 <31497908+KATC14@users.noreply.github.com>
2024-11-18 19:35:02 -0500

    Update color rating.js
    updated to reflect changes made to https://github.com/e621ng/e621ng

--- color rating.js
+++ color rating.js
@@ -12,54 +12,54 @@
 function load(){
   var flexcontainer = document.querySelector(".flex-container");
   if (flexcontainer){console.log("test", flexcontainer)} else {
-    var thestart = document.querySelectorAll("article.post-preview");
+    var thestart = document.querySelectorAll("article.thumbnail");
     for (var i = 0, max = thestart.length; i < max; i++) {
       var div = document.createElement("div");
       div.className = "flex-container";
       if (thestart[i].getAttribute("data-file-ext") == 'webm' || thestart[i].getAttribute("data-tags").search(/\banimated\b/) >= 0){
-        if (thestart[i].getAttribute("data-tags").includes('animated')) {var awtype = 'ANIM'}
-        if (thestart[i].getAttribute("data-file-ext") == 'webm') {var awtype = 'WEBM'}
-        div.innerHTML += `<span class="type-badge ${awtype.toLowerCase()}">${awtype}</span>`;
+        if (thestart[i].getAttribute("data-tags").includes('animated')) { var awtype = 'ANIM' }
+        if (thestart[i].getAttribute("data-file-ext") == 'webm') {        var awtype = 'WEBM' }
+        div.innerHTML += `<span class="mod_type-badge mod_${awtype.toLowerCase()}">${awtype}</span>`;
       }
     //}
     //for (var i = 0, max = thestart.length; i < max; i++) {
       if (thestart[i].getAttribute("data-rating") == 's') {var ratingtag = 'SAFE';}
       if (thestart[i].getAttribute("data-rating") == 'q') {var ratingtag = 'QUESTIONABLE';}
       if (thestart[i].getAttribute("data-rating") == 'e') {var ratingtag = 'EXPLICIT';}
-      div.innerHTML += `<span class="rating ${ratingtag.toLowerCase()}">${ratingtag}</span>`;
+      div.innerHTML += `<span class="mod_rating mod_${ratingtag.toLowerCase()}">${ratingtag}</span>`;
     //}
     //for (var i = 0, max = thestart.length; i < max; i++) {
       //var hm = thestart[i].childNodes[0].childNodes[0].childNodes[2]
 
       //console.log(ComputedStyle(hm, ['border-top-color','border-right-color','border-left-color','border-bottom-color']))
-      if (thestart[i].className.includes('post-status-flagged')) {
+      if (thestart[i].className.includes('flagged')) {
         inline(div, 'FLAGGED')
       }
-      if (thestart[i].className.includes('post-status-pending')) {
+      if (thestart[i].className.includes('pending')) {
         inline(div, 'PENDING')
       }
-      if (thestart[i].className.includes('post-status-has-parent')) {
+      if (thestart[i].className.includes('has-parent')) {
         inline(div, 'HAS-PARENT')
       }
-      if (thestart[i].className.includes('post-status-has-children')) {
+      if (thestart[i].className.includes('has-children')) {
         inline(div, 'HAS-CHILDREN')
       }
-      if (thestart[i].className.includes('post-status-deleted')) {
+      if (thestart[i].className.includes('deleted')) {
         inline(div, 'DELETED')
       }
       thestart[i].appendChild(div);
     }
   }
   var stylecode1 = `
-.webm, .anim{  background: #1f3c67}
-.safe{         background: #3e9e49}
-.questionable{ background: #e4e150}
-.explicit{     background: #e45f5f}
-.pending{      background: #0000ff}
-.has-parent{   background: #cccc00}
-.has-children{ background: #00ff00}
-.deleted{      background: #000000}
-.flagged{      background: #4d0000}
+.mod_webm, .mod_anim{  background: #1f3c67}
+.mod_safe{         background: #3e9e49}
+.mod_questionable{ background: #e4e150}
+.mod_explicit{     background: #e45f5f}
+.mod_pending{      background: #0000ff}
+.mod_has-parent{   background: #cccc00}
+.mod_has-children{ background: #00ff00}
+.mod_deleted{      background: #000000}
+.mod_flagged{      background: #4d0000}
 
 .flex-container{
   display: flex;
@@ -68,7 +68,7 @@ function load(){
   position: absolute;
   top: 10px;
 }
-.outline, .type-badge, .rating{
+.mod_outline, .mod_type-badge, .mod_rating{
   z-index: 5;
   color:#fff;
   text-align: center;
@@ -78,13 +78,13 @@ function load(){
   font-weight: 700;
   border: 1px solid #fff;
 }
-article.post-preview[data-file-ext='webm'] > a::before,
-article.post-preview[data-tags~='animated'] > a::before {
+article.thumbnail[data-file-ext='webm'] > a::before,
+article.thumbnail[data-tags~='animated'] > a::before {
   display:none;
 }`
   addGlobalStyle(stylecode1)
 }
 load()
 function inline(origin, outline){
-  origin.innerHTML += `<span class="outline ${outline.toLowerCase()}">${outline}</span>`;
+  origin.innerHTML += `<span class="mod_outline mod_${outline.toLowerCase()}">${outline}</span>`;
 }
